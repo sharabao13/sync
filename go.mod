7e10aeca654de81ed4b0d4061bf4683c2e18ed7e
Leslie Guan <37203069@qq.com>
2022-08-08 20:38:15 +0800

    上传文件接口

--- go.mod
+++ go.mod
@@ -4,7 +4,10 @@ go 1.18
 
 replace github.com/zserge/lorca v0.1.10 => C:\Users\Tae\Desktop\lorca
 
-require github.com/gin-gonic/gin v1.8.1
+require (
+	github.com/gin-gonic/gin v1.8.1
+	github.com/google/uuid v1.3.0
+)
 
 require (
 	github.com/gin-contrib/sse v0.1.0 // indirect
--- main.go
+++ main.go
@@ -3,17 +3,48 @@ package main
 import (
 	"embed"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"io/fs"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"os/exec"
 	"os/signal"
+	"path"
+	"path/filepath"
 	"strings"
 )
 
 //go:embed frontend/dist/*
 var FS embed.FS
+
+func TextsController(c *gin.Context) {
+	var json struct {
+		Raw string `json:"raw"`
+	}
+	if err := c.ShouldBindJSON(&json); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	} else {
+		exe, err := os.Executable() //获取当前执行文件的路径
+		if err != nil {
+			log.Fatal(err)
+		}
+		dir := filepath.Dir(exe)                 //获取当前执行文件的目录
+		filename := uuid.New().String()          //生成一个文件名
+		uploads := filepath.Join(dir, "uploads") //拼接upload绝对路径
+		err = os.MkdirAll(uploads, os.ModePerm)  //创建upload目录
+		if err != nil {
+			log.Fatal(err)
+		}
+		fullpath := path.Join("uploads", filename+".txt")                            //拼接文件的绝对路径 不含exe所在目录
+		err = ioutil.WriteFile(filepath.Join(dir, fullpath), []byte(json.Raw), 0644) //将json.raw写入文件
+		if err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, gin.H{"url": "/" + fullpath}) //返回文件的绝对路径(不含exe所在目录)
+	}
+}
 
 func main() {
 	go func() {
@@ -24,6 +55,7 @@ func main() {
 		//})
 		staticFiles, _ := fs.Sub(FS, "frontend/dist")
 		router.StaticFS("/static", http.FS(staticFiles))
+		router.POST("/api/v1/texts", TextsController)
 		router.NoRoute(func(c *gin.Context) {
 			path := c.Request.URL.Path
 			if strings.HasPrefix(path, "/static/") {
