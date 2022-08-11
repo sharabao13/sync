a914201254964eb5db4870fe6262b4d36d9e3348
Leslie Guan <37203069@qq.com>
2022-08-11 20:56:24 +0800

    完善退出功能

--- main.go
+++ main.go
@@ -8,25 +8,41 @@ import (
 	"os/signal"
 )
 
-func startBrowser() *exec.Cmd {
+func main() {
+	chChromDie := make(chan struct{})
+	chBackendDie := make(chan struct{})
+	go server.Run()
+	go startBrowser(chChromDie, chBackendDie)
+	chSignal := listenToInterrupt()
+	for {
+		select {
+		case <-chSignal:
+			chBackendDie <- struct{}{}
+
+		case <-chChromDie:
+			os.Exit(0)
+		}
+	}
+
+}
+
+func startBrowser(chChromDie chan struct{}, chBackendDie chan struct{}) {
 	chromePath := "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
 	cmd := exec.Command(chromePath, "--app=http://localhost:"+config.GetPort()+"/static/index.html")
 	cmd.Start()
-	return cmd
+	go func() {
+		<-chBackendDie
+		cmd.Process.Kill()
+	}()
+	go func() {
+		cmd.Wait()
+		chChromDie <- struct{}{}
+	}()
+
 }
 
 func listenToInterrupt() chan os.Signal {
 	chSignal := make(chan os.Signal, 1)
 	signal.Notify(chSignal, os.Interrupt)
 	return chSignal
-}
-func main() {
-	go server.Run()
-	cmd := startBrowser()
-	chSignal := listenToInterrupt()
-
-	select {
-	case <-chSignal:
-		cmd.Process.Kill()
-	}
 }
